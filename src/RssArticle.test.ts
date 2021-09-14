a24de166c35cf436f91e612725bb33d7958a792b
MH4GF <rixi436@gmail.com>
2021-09-14 20:33:05 +0900

    add todo test

--- src/RssArticle.test.ts
+++ src/RssArticle.test.ts
@@ -2,6 +2,11 @@ import { RssArticle } from './RssArticle';
 
 describe('RssArticle', () => {
   describe('#fetchArticles()', () => {
+    describe('正常系', () => {
+      it.todo('RSSのURLにリクエストした結果が空の場合、空配列が返る');
+      it.todo('RSSのURLにリクエストした結果が1つ以上存在する場合、Articleの配列が返る');
+    });
+
     describe('異常系', () => {
       it('urlが空文字の場合エラーが返る', async () => {
         const rssArticle = new RssArticle();
@@ -10,6 +15,9 @@ describe('RssArticle', () => {
           new Error('connect ECONNREFUSED 127.0.0.1:80')
         );
       });
+
+      it.todo('urlが不正な値の場合エラーが返る');
+      it.todo('URLへのリクエストに失敗した場合、hogehoge');
     });
   });
 });
